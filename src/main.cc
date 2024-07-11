31527689babab44be72812ad5cd63294189283a7
Robert <robert11122002@gmail.com>
2024-07-11 13:40:18 +0100

    added a simple use case so my clangd works with the templates

--- src/main.cc
+++ src/main.cc
@@ -8,7 +8,7 @@ struct Foo {
   explicit Foo(int x) {}
 };
 
-auto main() -> int {
+auto main() -> int try {
   FormatArgs<const char*, int, int, Foo> args{"hello world", 42, 42, Foo(42)};
 
   auto item = std::get<2>(args);
@@ -18,8 +18,12 @@ auto main() -> int {
   f[0] = ReplacementSpan{0, 1};
   f.emplace<0>({2, 3});
 
-  // auto s = format::format("Hello world {}", 42);
-  std::cout << "Size: " << args.estimate_size() << '\n';
+  auto s =
+      format::format("Hello world {}, and another {} {}", 42, "word", 34.22);
+  std::cout << s << '\n';
 
   return 0;
+} catch (const std::exception& e) {
+  std::cerr << e.what() << '\n';
+  return 1;
 }
