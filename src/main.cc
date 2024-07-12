fc9e2c6bbd8b0a45ae6cb560d5fcdb0d5ac93a96
Robert <robert11122002@gmail.com>
2024-07-12 14:11:32 +0100

    ..

--- src/main.cc
+++ src/main.cc
@@ -2,28 +2,27 @@
 
 #include "format/format.hpp"
 
-using format::ReplacementList, format::FormatArgs, format::ReplacementSpan;
-
 struct Foo {
   explicit Foo(int x) {}
 };
 
-constexpr inline auto FmtStr{
-    format::FormatString<int, int>("Hello world, {}, {}")};
+constexpr inline auto FmtStr{format::FormatString<int>("Hello world {0:010x}")};
 
 auto main() -> int try {
-  FormatArgs<const char*, int, int, Foo> args{"hello world", 42, 42, Foo(42)};
-
-  auto item = std::get<2>(args);
-
-  ReplacementList<1> f{};
-
-  f[0] = ReplacementSpan{0, 1};
-  f.emplace<0>({2, 3});
-
   auto s =
       format::format("Hello world {}, and another {} {}", 42, "word", 34.22);
   std::cout << s << '\n';
+
+  const auto& specifiers{FmtStr.get_arg_specifiers()};
+
+  for (const auto& specifier : specifiers) {
+    ::std::cerr << "Specifiers: " << specifier.specifiers_
+                << "\nFill: " << specifier.fill_
+                << "\nHas position: " << specifier.has_position_
+                << "\nPosition: " << specifier.position_
+                << "\nSize: " << specifier.size_
+                << "\nHas size: " << specifier.has_size_ << "\n";
+  }
 
   return 0;
 } catch (const std::exception& e) {
